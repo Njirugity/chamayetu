7a68bd2d44d44b1dac14a2c355dde18870a85047
brian <muriithibrian283@gmail.com>
2025-03-16 12:00:44 +0300

    Backend folder

---
+++ backend/src/main/java/com/chamayetu/frinance/FrinanceApplication.java
@@ -0,0 +1,13 @@
+package com.chamayetu.frinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrinanceApplication.class, args);
+	}
+
+}
---
+++ chamayetu/src/pages/Members/RegistrationForm.tsx
@@ -0,0 +1,9 @@
+import { use, useState } from "react";
+
+function Registration() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [id, setId] = useState("");
+  const [contact, setContact] = useState("");
+  const [email, setEmail] = useState("");
+}
