d996ba6d128b4b889f77d917fffc5e651251c582
Denis <denismunene999@gmail.com>
2025-03-20 09:09:24 +0300

    Upgrade spring

--- chamayetu-backend/src/main/java/com/chamayetu/chamayetu/model/LoansRepayment.java
+++ chamayetu-backend/src/main/java/com/chamayetu/chamayetu/model/LoansRepayment.java
@@ -1,9 +1,9 @@
 package com.chamayetu.chamayetu.model;
 
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
