0cdaa204f01cacacfb0bd960e2348ee009b11415
Denis <denismunene999@gmail.com>
2025-04-02 23:08:56 +0300

    Map loans data to loans table and add new loan submission logic

--- chamayetu/src/models/Loans.ts
+++ chamayetu/src/models/Loans.ts
@@ -1,7 +1,18 @@
 export type Loan = {
-  first_name: string;
-  last_name: string;
   member_id: string;
-  due_date: string;
-  amount: number;
+  repayment_date: Date;
+  loan_amount: number;
 };
+
+export type LoanDashboard = {
+  created_at: string;      
+  modified_at: string;      
+  loan_id: number;          
+  member_id: string;        
+  due_date: string;         
+  amount: number;           
+  loan_status: string;     
+  loan_balance: number;     
+  interest_rate: number; 
+}
+
